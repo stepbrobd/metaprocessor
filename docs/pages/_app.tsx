e07000cf8671bceab38768b2547607bce65fc376
Yifei Sun <ysun@hey.com>
2023-03-05 18:28:04 -0700

    change mdx _app to tsx _app

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "styles/tailwind.css";
 
+import { AppProps } from "next/app";
 import Script from "next/script";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
