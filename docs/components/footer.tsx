80859b56369c7cd5b7ff97ef552b5404b950f825
StepBroBD <Hi@StepBroBD.com>
2023-03-05 20:43:21 -0700

    add commit digest section

--- docs/components/footer.tsx
+++ docs/components/footer.tsx
@@ -1,11 +1,24 @@
 import Logo from "components/logo";
 
 const Footer = () => {
+  // vercel detected commit hash or fallback to initial commit hash
+  const commit =
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA ||
+    "bf64b3f38296fe9b953b3f795be2259fa01571b1";
+
   return (
-    <div className="flex flex-col items-center sm:items-start space-y-4">
+    <div className="flex flex-col items-center sm:items-start space-y-6">
       <Logo />
       <small>
-        &copy; {new Date().getFullYear()}{" "}
+        <a
+          href={`https://github.com/metaprocessor/metaprocessor/commit/${commit}`}
+          target="_blank"
+        >
+          Commit digest: <u className="font-mono">{commit.substring(0, 17)}</u>.
+        </a>
+      </small>
+      <small>
+        Copyright &copy; {new Date().getFullYear()}{" "}
         <a
           href="https://github.com/metaprocessor/metaprocessor/graphs/contributors"
           target="_blank"
