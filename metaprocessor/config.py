881c69919377e52d7eb81000486d53f7317c6693
StepBroBD <Hi@StepBroBD.com>
2023-04-09 22:16:48 -0600

    refactored app to use click instead of argparse

--- metaprocessor/config.py
+++ metaprocessor/config.py
@@ -1,19 +1,37 @@
-import toml
+import os
 import pathlib
+import toml
 
 
-def exist_config() -> bool:
-    return pathlib.Path.home().joinpath(".config", "metaprocessor", "config.toml").exists()
+def location() -> str:
+    """
+    Check `XDG_CONFIG_HOME` environment variable,
+    if it is not set, use `~/.config` instead.
+    """
+    return pathlib.Path(
+        os.environ.get(
+            "XDG_CONFIG_HOME",
+            pathlib.Path.home().joinpath(".config"),
+        )
+    )/"metaprocessor"/"config.toml"
 
 
-def read_config() -> dict:
-    return toml.load(pathlib.Path.home().joinpath(".config", "metaprocessor", "config.toml"))
+def exist() -> bool:
+    return pathlib.Path(location()).exists()
 
 
-def write_config(config: dict) -> None:
-    file = pathlib.Path.home().joinpath(".config", "metaprocessor", "config.toml")
+def edit() -> None:
+    os.system(f"{os.environ.get('EDITOR', 'vim')} {pathlib.Path(location())}")
+
+
+def read() -> dict:
+    return toml.load(pathlib.Path(location()))
+
+
+def write(config: dict) -> None:
+    file = location()
     file.parent.parent.mkdir(parents=True, exist_ok=True)
     file.parent.mkdir(parents=True, exist_ok=True)
     file.touch(exist_ok=True)
-    with open(pathlib.Path.home().joinpath(".config", "metaprocessor", "config.toml"), "w") as f:
+    with open(file, "w") as f:
         toml.dump(config, f)
