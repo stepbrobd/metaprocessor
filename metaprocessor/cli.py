842966cdb5bf2b219c086adf77c84c4d04e0bb54
Yifei Sun <ysun@hey.com>
2023-07-31 11:14:56 -0600

    add shell completion

--- metaprocessor/cli.py
+++ metaprocessor/cli.py
@@ -1,8 +1,10 @@
 import click
 
+from metaprocessor.commands.completion import completion
 from metaprocessor.commands.config import config
 from metaprocessor.commands.metawear import metawear
-from metaprocessor.commands.objects import objects
+from metaprocessor.commands.object import object
+from metaprocessor.commands.run import run
 
 
 @click.group()
@@ -14,6 +16,8 @@ def cli() -> None:
     pass
 
 
+cli.add_command(completion)
 cli.add_command(config)
 cli.add_command(metawear)
-cli.add_command(objects)
+cli.add_command(object)
+cli.add_command(run)
