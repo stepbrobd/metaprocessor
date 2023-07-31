5458e2c799c86445c9443492558d8476f21fa9d1
Yifei Sun <ysun@hey.com>
2023-07-31 11:16:03 -0600

    add linux_only decorator

--- metaprocessor/commands/metawear.py
+++ metaprocessor/commands/metawear.py
@@ -1,5 +1,7 @@
 import click
 
+from metaprocessor.helpers.decorator import linux_only
+
 
 @click.group()
 def metawear() -> None:
@@ -10,14 +12,15 @@ def metawear() -> None:
 
 
 @metawear.command()
+@linux_only
 def download() -> None:
     """
     Download raw IMU data from MetaWear.
     """
     pass
 
-
 @metawear.command()
+@linux_only
 def reset() -> None:
     """
     Reset MetaWear to factory settings.
