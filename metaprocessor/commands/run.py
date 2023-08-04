6fb7f7bbbb5f505793202ef20feb2b2593f486da
Yifei Sun <ysun@hey.com>
2023-08-04 15:12:51 -0600

    run: add preprocessor workflow

--- metaprocessor/commands/run.py
+++ metaprocessor/commands/run.py
@@ -1,4 +1,11 @@
+import pathlib
+from concurrent.futures.thread import ThreadPoolExecutor
+
 import click
+from rich import print
+
+import metaprocessor.helpers.config
+import metaprocessor.helpers.workflow
 
 
 @click.group()
@@ -10,8 +17,30 @@ def run() -> None:
 
 
 @run.command()
-def preprocess() -> None:
+@click.option(
+    "--key",
+    required=False,
+    help="Key of the session to be processed.",
+)
+def preprocess(key: str) -> None:
     """
     Preprocess raw IMU data.
     """
-    pass
+    config = metaprocessor.helpers.config.read()
+
+    if key:
+        tasks = [ pathlib.Path(config["general"]["gd-location"])/key ]
+    else:
+        tasks = metaprocessor.helpers.workflow.generate_tasks(config)
+
+    for task in tasks:
+        if not task.exists():
+            print(f"[red]Provided session file [u]{task}[/u] does not exist.[/red]")
+            raise SystemExit(1)
+
+    with ThreadPoolExecutor() as executor:
+        executor.map(
+            metaprocessor.helpers.workflow.preprocess,
+            tasks,
+            [config]*len(tasks),
+        )
