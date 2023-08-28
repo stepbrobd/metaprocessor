482d06fe3d42a80f89d973ef6043c2de01567527
StepBroBD <Hi@StepBroBD.com>
2023-08-27 20:35:19 -0400

    fix preprocessor workflow, add uptime + steps calculation, initial set up for report generation

--- metaprocessor/helpers/workflow.py
+++ metaprocessor/helpers/workflow.py
@@ -3,9 +3,13 @@ import pathlib
 import re
 import time
 import zipfile
+from typing import List
 
 import pandas as pd
 from rich import print
+
+from metaprocessor.libs.steps import steps as calculate_steps
+from metaprocessor.libs.uptime import uptime as calculate_uptime
 
 
 def generate_tasks(config: dict) -> list:
@@ -33,19 +37,18 @@ def get_timestamp(input: str, offset: float) -> int:
 
 def align_timestamp(df: pd.DataFrame, start: int, end: int) -> pd.DataFrame:
     df["epoc (ms)"] = df["epoc (ms)"].astype(int)
-    df = df.sort_values(by=["epoc (ms)"], ascending=True)
+    df = df.sort_values(by="epoc (ms)", ascending=True)
 
-    diff = start - df.iloc[0]["epoc (ms)"]
-    df["epoc (ms)"] = df["epoc (ms)"].apply(lambda x: x + diff)
+    diff = start - df["epoc (ms)"].iloc[0]
+    df["epoc (ms)"] = df["epoc (ms)"] + diff
 
     if end > df.iloc[-1]["epoc (ms)"]:
-        cutoff_index = -1
+        cutoff = -1
     else:
-        cutoff_index = df[df["epoc (ms)"] > end].index[0]
+        cutoff = df[df["epoc (ms)"] > end].index[0]
+    df = df.iloc[:cutoff]
 
-    df = df.iloc[:cutoff_index]
-
-    return df
+    return df.sort_values(by="epoc (ms)", ascending=True)
 
 
 def merge(acce: pathlib.Path, gyro: pathlib.Path, start: int, end: int) -> pd.DataFrame:
@@ -80,25 +83,38 @@ def merge(acce: pathlib.Path, gyro: pathlib.Path, start: int, end: int) -> pd.Da
     return df
 
 
-def get_preprocessed(task: pathlib.Path) -> pathlib.Path:
+def get_preprocessed(task: pathlib.Path) -> pd.DataFrame:
     folder = pathlib.Path(task.as_posix().split(".zip")[0])
     if not folder.exists():
         raise FileNotFoundError("No preprocessed data found")
 
     if folder.glob("-Preprocessed.feather"):
         print(f"Using {folder/(folder.name+'-Preprocessed.feather')}")
-        return folder/(folder.name+"-Preprocessed.feather")
+        return pd.read_feather(folder/(folder.name+"-Preprocessed.feather"))
     elif folder.glob("-Preprocessed.csv"):
         print(f"Using {folder/(folder.name+'-Preprocessed.csv')}")
-        return folder/(folder.name+"-Preprocessed.csv")
+        return pd.read_csv(folder/(folder.name+"-Preprocessed.csv"))
     else:
         raise FileNotFoundError("No preprocessed data found")
 
 
-def get_split_indices(task: pathlib.Path, config: dict) -> list:
-    df = pd.read_feather(task)
-    if len(df) < 10000:
-        print(task, df.shape)
+def remove_milliseconds(epoch: int) -> int:
+    if len(str(epoch)) == 13:
+        return int(str(epoch)[:-3])
+    elif len(str(epoch)) == 10:
+        return epoch
+    else:
+        raise ValueError("Epoch must be in milliseconds or seconds")
+
+
+def get_split_indices(df: pd.DataFrame) -> list:
+    """
+    Return a list of tuples containing the start and end indices of each split.
+    """
+    df["datetime"] = pd.to_datetime(df["epoc (ms)"], unit="ms")
+    grouped = df.groupby(df["datetime"].dt.date).apply(lambda x: (x.index.min(), x.index.max()))
+    df.drop(columns=["datetime"], inplace=True)
+    return grouped.tolist()
 
 
 def preprocess(task: pathlib.Path, config: dict) -> None:
@@ -127,10 +143,87 @@ def preprocess(task: pathlib.Path, config: dict) -> None:
 
 
 def uptime(task: pathlib.Path, config: dict) -> None:
-    file = get_preprocessed(task)
-    # data = pd.read_feather(file)
-    get_split_indices(file, config)
+    folder = pathlib.Path(task.as_posix().split(".zip")[0])
+    output = folder/(folder.name+"-Results.csv")
+
+    df = get_preprocessed(task)
+    indices = get_split_indices(df)
+
+    # columns will be uptime, steps, etc.
+    results = pd.DataFrame(columns=["uptime (%)"])
+
+    for index, (start, end) in enumerate(indices):
+        calc = df.iloc[start:end]
+        calc.reset_index(drop=True, inplace=True)
+        result = calculate_uptime(calc, gain=1, sample_rate=25, critical_angle=39)
+        print(f"\t[green]{folder.name} Day ({index+1}/{len(indices)})[/green]: UpTime: {result}")
+        results.loc[index, "uptime (%)"] = result
+
+    if output.exists():
+        result_df = pd.read_csv(output)
+        result_df["uptime (%)"] = results["uptime (%)"]
+    else:
+        result_df = results
+
+    result_df.to_csv(output, index=False)
+
+    print(f"[green]Calculated uptime for {folder.name}[/green]")
 
 
 def steps(task: pathlib.Path, config: dict) -> None:
-    pass
+    folder = pathlib.Path(task.as_posix().split(".zip")[0])
+    output = folder/(folder.name+"-Results.csv")
+
+    df = get_preprocessed(task)
+    indices = get_split_indices(df)
+
+     # columns will be uptime, steps, etc.
+    results = pd.DataFrame(columns=["steps (count)"])
+
+    for index, (start, end) in enumerate(indices):
+        calc = df.iloc[start:end]
+        calc.reset_index(drop=True, inplace=True)
+        result = calculate_steps(calc)
+        print(f"\t[green]{folder.name} Day ({index+1}/{len(indices)})[/green]: Steps: {result}")
+        results.loc[index, "steps (count)"] = result
+
+    if output.exists():
+        result_df = pd.read_csv(output)
+        result_df["steps (count)"] = results["steps (count)"]
+    else:
+        result_df = results
+
+    result_df.to_csv(output, index=False)
+
+    print(f"[green]Calculated steps for {folder.name}[/green]")
+
+
+def report(tasks: List[pathlib.Path]) -> None:
+    results = []
+    for task in tasks:
+        folder = pathlib.Path(task.as_posix().split(".zip")[0])
+        output = folder/(folder.name+"-Results.csv")
+        if output.exists():
+            results.append(output)
+    if len(results) == 0:
+        print("[red]No results found.[/red]")
+        return
+
+    print(f"[green]Generating report for {len(results)} session(s).[/green]")
+
+    # get all column names from all results
+    columns = []
+    for result in results:
+        df = pd.read_csv(result)
+        columns.extend(df.columns.tolist())
+    columns = list(set(columns))
+
+    # id, day_1, day_2, day_3, ..., day_99
+    result_template = pd.DataFrame(columns=["id"] + [f"day_{i}" for i in range(1, 100)])
+
+    # key is the column name, value is dataframe
+    reports = {}
+    for column in columns:
+        reports[column] = result_template.copy()
+
+    # TODO
